76fe0bd1be163d5a1f98855422d21862d979e140
Teemah36 <fatimamuhammad495@gmail.com>
2024-06-10 13:39:44 +0100

    create and update object

--- week2/object.js
+++ week2/object.js
@@ -1,2 +1,16 @@
 // const date = new Date();
 // console.log(date);
+
+const tables = {
+    noOfLegs: 4,
+    color: 'white',
+    material: 'plastics',
+    isFoldable: true,
+    height: 0.7,
+}
+
+console.log(tables.isFoldable);
+tables['colorOfLegs'] = 'grey';
+tables['width'] = 1.2
+ 
+console.log(tables.colorOfLegs + ' ' + tables.material + ' ' + tables.width)
